dbd612c80c173b1e5ada3a8afb8e1ea7cc18262d
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2023-10-19 16:59:57 +0800

    类、样式、计算属性、响应数据、方法

--- learn/section-2-2/app.js
+++ learn/section-2-2/app.js
@@ -3,11 +3,15 @@ const vm = Vue.createApp({
         return {
             isPurple: false,
             selectedColor: '',
+            size:150
         }
     },
     computed: {
         circle_classes() {
             return {purple: this.isPurple}
+        },
+        circle_styles(){
+            return [{width:this.size+'px',height:this.size+'px',lineHeight:this.size+'px'},{transform:'rotate(30deg)'}]
         }
     }
 }).mount('#app')
---
+++ learn/section-2-3/app.js
@@ -0,0 +1,20 @@
+const vm = Vue.createApp({
+    data() {
+        return {
+            mode:1
+        }
+    }
+}).mount('#app')
+
+// setTimeout(() => {
+//     vm.firstName = 'LeBron';
+// }, 2000)
+
+// Vue.createApp({
+//     data(){
+//         return {
+//             firstName:'John',
+//             lastName:'Doe'
+//         }
+//     }
+// }).mount('#app1')
