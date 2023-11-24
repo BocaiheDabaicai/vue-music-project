7dbefae25a3543cc94d6c24ff0bfaa508ebf516c
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2023-11-24 10:37:59 +0800

    过渡、动画、JS过渡

---
+++ 4-animation-project/animations/src/App.vue
@@ -0,0 +1,123 @@
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      flag: false
+    }
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter event fired ", el)
+    },
+    enter(el, done) {
+      console.log("enter event fired ", el)
+
+      const animation = el.animate(
+          [{transform: "scale3D(0,0,0)"}, {}], {duration: 1000}
+      )
+      animation.onfinish = () => done()
+    },
+    afterEnter(el) {
+      console.log("afterEnter event fired ", el)
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave event fired ", el)
+    },
+    leave(el, done) {
+      console.log("leave event fired ", el)
+      done()
+    },
+    afterLeave(el) {
+      console.log("afterLeave event fired ", el)
+    },
+  }
+}
+
+</script>
+
+<template>
+  <button type="button" @click="flag=!flag">Toggle</button>
+
+<!--  <transition name="fade" mode="out-in">-->
+<!--    <h2 v-if="flag" key="main">Hello World!</h2>-->
+<!--    <h2 v-else key="secondary">Another World!</h2>-->
+<!--  </transition>-->
+
+<!--  <hr/>-->
+
+<!--  <transition name="zoom" type="animation">-->
+<!--    <h2 v-if="flag">Hello</h2>-->
+<!--  </transition>-->
+
+<!--  <hr/>-->
+
+  <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+  >
+    <h2 v-if="flag">Hey</h2>
+  </transition>
+</template>
+
+<style scoped>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: 1s all linear;
+}
+
+.fade-leave-to {
+  transition: 1s all linear;
+  opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+  transition: 1s all linear;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+  transition: 1s all linear
+}
+
+.zoom-enter-from {
+  opacity: 0;
+}
+
+.zoom-leave-to {
+  opacity: 0;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
+}
+
+</style>
